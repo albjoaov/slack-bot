25e093d2973631b97ef6bb06481b3620ba701dab
João Albuquerque <albjoaov@gmail.com>
2023-03-06 17:53:22 -0300

    chore(main): remove command in build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ set -o errexit
 curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
 export PATH=/opt/render/.local/bin:$PATH
 pdm install --production --verbose
-
-python app.py
